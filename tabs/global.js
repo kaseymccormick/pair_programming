fb677827ce76834d1edae4f063337f959fe8bf2d
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-08 13:28:09 -0500

    Switching computer..........

--- tabs/global.js
+++ tabs/global.js
@@ -3,24 +3,41 @@
 // var test = document.getElementById("tab1").className += ' hide';
 
 
-document.getElementById("tab1").onclick = hideTabs("tab1");
+// function hideTabs(selected){
+//   if (selected === "tab1"){
+//     document.getElementById("tab2").className += ' hide';
+//     document.getElementById("tab3").className += ' hide';
+//     //hide tab2 and tab3
+//   } else if (selected === "tab2"){
+//     document.getElementById("tab1").className += ' hide';
+//     document.getElementById("tab3").className += ' hide';
+//     //hide tab1 and tab3
+//   } else if{
+//     document.getElementById("tab1").className += ' hide';
+//     document.getElementById("tab2").className += ' hide';
+//     //hide tab1 and tab2
+//   }
+// }
 
-document.getElementById("tab2").onclick = hideTabs;
-
-document.getElementById("tab3").onclick = hideTabs;
-
-function hideTabs(selected){
-  if (selected === "tab1"){
-    document.getElementById("tab2").className += ' hide';
-    document.getElementById("tab3").className += ' hide';
-    //hide tab2 and tab3
-  } else if (selected === "tab2"){
-    document.getElementById("tab1").className += ' hide';
-    document.getElementById("tab3").className += ' hide';
-    //hide tab1 and tab3
-  } else {
-    document.getElementById("tab1").className += ' hide';
-    document.getElementById("tab2").className += ' hide';
-    //hide tab1 and tab2
-  }
+function hideTabs1(){
+  document.getElementById("tab2Content").className += ' hide';
+  document.getElementById("tab3Content").className += ' hide';
 }
+
+function hideTabs2(){
+  document.getElementById("tab1Content").className += ' hide';
+  document.getElementById("tab3Content").className += ' hide';
+}
+
+function hideTab3(){
+  document.getElementById("tab1Content").className += ' hide';
+  document.getElementById("tab2Content").className += ' hide';
+}
+
+hideTabs1();
+
+document.getElementById("tab1").onclick = hideTabs1;
+
+document.getElementById("tab2").onclick = hideTabs2;
+
+document.getElementById("tab3").onclick = hideTabs3;
