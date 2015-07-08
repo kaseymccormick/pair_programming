da3292a73dcd3dd8a4b2aae0535c28945562937a
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-08 12:24:49 -0500

    So close to making work!!!!!!

--- tabs/global.js
+++ tabs/global.js
@@ -1,39 +1,26 @@
-// not sure what using for at the moment, hold
-// var tablinks = ["tab1", "tab2 ", "tab3"];
-// var contentDivs = ["you're on tab1", "tab two!","tab four, just kidding"]
 
-function showTab(){
-  //show div partner to the tab selected and hide the other two//
-  when i click on tab 1 add class or id 'hidden' to tab content div's' 2 and 3
-}
 
-function showTab() {
-  var selectedId = getHash( this.getAttribute('href') );
+// var test = document.getElementById("tab1").className += ' hide';
 
-  // Highlight the selected tab, and dim all others.
-  // Also show the selected content div, and hide all others.
-  for ( var id in contentDivs ) {
-    if ( id == selectedId ) {
-      tabLinks[id].className = 'selected';
-      contentDivs[id].className = 'tabContent';
-    } else {
-      tabLinks[id].className = '';
-      contentDivs[id].className = 'tabContent hide';
-    }
+
+document.getElementById("tab1").onclick = hideTabs("tab1");
+
+document.getElementById("tab2").onclick = hideTabs;
+
+document.getElementById("tab3").onclick = hideTabs;
+
+function hideTabs(selected){
+  if (selected === "tab1"){
+    document.getElementById("tab2").className += ' hide';
+    document.getElementById("tab3").className += ' hide';
+    //hide tab2 and tab3
+  } else if (selected === "tab2"){
+    document.getElementById("tab1").className += ' hide';
+    document.getElementById("tab3").className += ' hide';
+    //hide tab1 and tab3
+  } else {
+    document.getElementById("tab1").className += ' hide';
+    document.getElementById("tab2").className += ' hide';
+    //hide tab1 and tab2
   }
-
-  // Stop the browser following the link
-  return false;
-}
-
-
-document.getElementById("tab1").onclick = showTab
-
-document.getElementById("tab2").onclick = showTab
-
-document.getElementById("tab3").onclick = showTab
-
-function all_theStuff(id){
-document.getElementsByClassName("tabContent");
-document.getElementById("id");
 }
